c64d02e983d2eca88234a69cd0aaf68ca6f39fc4
Hamish Currie <hamish@currie.com.au>
2012-12-30 19:26:16 +1000

    getting ready for ci

--- test_basic.py
+++ test_basic.py
@@ -47,16 +47,21 @@ class GraphFetchTests(unittest.TestCase):
         foo = fetch(fd, key_filter=Foo.name=='joe')[0]
         self.assertEqual(foo.name, 'joe', "name incorrect")
 
-
-if __name__ == '__main__':
-    # Add the appengine sdk to the python path. Note that this is very specific to my machine (a mac)
-    dev_appserver = '/usr/local/bin/dev_appserver.py'
-    dev_appserver_realpath = os.path.realpath(dev_appserver)
-    dev_appserver_realdir = os.path.split(dev_appserver_realpath)[0]
-    logging.info("adding path: %s" % dev_appserver_realdir)
-    sys.path.append(dev_appserver_realdir)
-    sys.path.append("%s/%s" % (dev_appserver_realdir , 'yaml/lib'))
-    
-    
-    unittest.main()
+    def testBasicKeyList(self):
+        
+        fd = FetchDefinition(Foo)
+        foo = fetch(fd, key_filter=Foo.name=='joe')[0]
+        self.assertEqual(foo.name, 'joe', "name incorrect")
+#
+#if __name__ == '__main__':
+#    # Add the appengine sdk to the python path. Note that this is very specific to my machine (a mac)
+#    dev_appserver = '/usr/local/bin/dev_appserver.py'
+#    dev_appserver_realpath = os.path.realpath(dev_appserver)
+#    dev_appserver_realdir = os.path.split(dev_appserver_realpath)[0]
+#    logging.info("adding path: %s" % dev_appserver_realdir)
+#    sys.path.append(dev_appserver_realdir)
+#    sys.path.append("%s/%s" % (dev_appserver_realdir , 'yaml/lib'))
+#    
+#    
+#    unittest.main()
 
